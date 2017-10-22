06467c17fa1bd0d69c3798f3f81d1354c99730b2
Thomas ltp <tle@tlnk.fr>
2017-10-22 13:54:38 +0200

    update for php 7.0

--- entrypoint/entrypoint.d/1.phpConfig.sh
+++ entrypoint/entrypoint.d/1.phpConfig.sh
@@ -6,7 +6,7 @@ PHP_MAX_EXCUTION_TIME="${PHP_MAX_EXCUTION_TIME:=200}"
 PHP_MEMORY_LIMIT="${PHP_MEMORY_LIMIT:=256M}"
 
 #Set parameters
-sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/g" /etc/php5/apache2/php.ini && \
-sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = $PHP_POST_MAX_SIZE/g" /etc/php5/apache2/php.ini && \
-sed -i -e "s/max_execution_time\s*=\s*30/max_execution_time = $PHP_MAX_EXCUTION_TIME/g" /etc/php5/apache2/php.ini && \
-sed -i -e "s/memory_limit\s*=\s*128M/memory_limit = $PHP_MEMORY_LIMIT/g" /etc/php5/apache2/php.ini
+sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/g" /etc/php/7.0/apache2/php.ini
+sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = $PHP_POST_MAX_SIZE/g" /etc/php/7.0/apache2/php.ini
+sed -i -e "s/max_execution_time\s*=\s*30/max_execution_time = $PHP_MAX_EXCUTION_TIME/g" /etc/php/7.0/apache2/php.ini
+sed -i -e "s/memory_limit\s*=\s*128M/memory_limit = $PHP_MEMORY_LIMIT/g" /etc/php/7.0/apache2/php.ini
